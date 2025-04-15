3f6c30e7efd58f80ca7bd368520e3ca8ac35ab97
Muhammad Zain <zain.ashiq613@gmail.com>
2025-04-15 18:00:42 +0500

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <h2>
+        <img src="./dummy logo.png" /> Rest Room Management
+      </h2>
+    </div>
+  );
+}
