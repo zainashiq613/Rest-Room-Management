930a830ac8ddb9bb95382391233b55db32d91ff8
Muhammad Zain <zain.ashiq613@gmail.com>
2025-04-25 18:09:32 +0500

    asasasssssss

--- src/components/PagesTop.jsx
+++ src/components/PagesTop.jsx
@@ -7,7 +7,7 @@ import shape from "./images/Shape.png";
 export default function DashboardTop({ title }) {
   return (
     <div
-      className="flex w-[100%] h-[20vh] lg:h-[27vh] py-3 px-2 justify-between items-center lg:items-start sm:py-4 sm:px-6 lg:relative"
+      className="flex w-[100%] h-[15vh] lg:h-[27vh] py-3 px-2 justify-between items-center lg:items-start sm:py-4 sm:px-6 lg:relative"
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
       <h1 className="text-white text-2xl font-bold self-end">{title}</h1>
